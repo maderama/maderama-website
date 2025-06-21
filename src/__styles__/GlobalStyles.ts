a19a11d2153c0fac366ea873730d282fff851cf2
Madison Beare <madisonbeare@gmail.com>
2025-06-21 22:11:10 +1000

    feature | set up background and grid, and create basic ripple effect

---
+++ src/__styles__/GlobalStyles.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    font-family: 'Instrument Serif', serif;
+    line-height: 1.5;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: rgba(0, 0, 0, 0.87);
+    background-color: #f8f8f8;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background-size: 30px 30px;
+    background-image:
+        linear-gradient(to right, #dddddd 1px, transparent 1px),
+        linear-gradient(to bottom, #dddddd 1px, transparent 1px);
+  }
+
+  #root {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+`;
